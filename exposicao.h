06ca37a70519aa73b07a6472d204810e3e252fba
Cauã Martins <131932004+cauacodigos@users.noreply.github.com>
2024-01-15 02:01:59 -0300

    Create exposicao.h

---
+++ exposicao.h
@@ -0,0 +1,12 @@
+#ifndef EXPOSICAO_H_INCLUDED
+#define EXPOSICAO_H_INCLUDED
+
+void bem_vindo();
+void printseparator();
+void art(int informacao);
+void santos_dumont();
+void olympcs();
+void rings();
+void porto (int informacao);
+void question (int satisf, char esc [4][5] , char nome[4][30], int quantidade, char obra [60]);
+#endif // EXPOSICAO_H_INCLUDED
